2d7b85b695d6e3e26312a35f21ebd991afb843fb
Jan-Hendrik Willms <tleilax+github@gmail.com>
2014-02-06 21:56:01 +0100

    dismiss messages only on button click (resembles MessageBox behaviour)

--- assets/messa.js
+++ assets/messa.js
@@ -1,7 +1,7 @@
 (function () {
     $.noty.defaults.layout = 'inline';
     $.noty.defaults.theme  = 'studipTheme';
-//    $.noty.defaults.closeWith  = ['button'];
+    $.noty.defaults.closeWith  = ['button'];
     
     var container = '#barBottomContainer';
 
